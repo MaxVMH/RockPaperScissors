b05eedfe4b71cc2f1ae76808a461cae22c4b3b68
Maxim <MaxVMH@github>
2020-06-16 17:49:57 +0200

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Maxim Van Meerhaeghe on 16/06/2020.
+//  Copyright © 2020 Mixam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
